2b4460facb7076c8a356d7cb1763aad4c74f9ad0
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-07-23 01:32:39 +0100

    Initial Commit

---
+++ src/lib/content/experience.ts
@@ -0,0 +1,118 @@
+import { ExperienceSectionType } from '@/lib/types/sections';
+
+export const experienceSection: ExperienceSectionType = {
+  title: "where i've worked",
+  experiences: [
+    {
+      company: 'Rawsteal',
+      companyUrl: '#',
+      role: 'Moderation',
+      started: 'December 2021',
+      upto: 'February 2022',
+      tasks: [
+        'Issued over 250 punishments to users who broke the rules.',
+        'Developed and Maintained trust within the community.',
+        'Helped to build a community of over 2000 people.',
+      ],
+    },
+    {
+      company: 'KeepInvBox',
+      companyUrl: '#',
+      role: 'Jr Developer - Owner',
+      started: 'February 2022',
+      upto: 'Present',
+      tasks: [
+        'Refactored the codebase from skript into Java & Kotlin',
+        'Maintained and expanded the community to 1000 members.',
+        'Collaborated with numerous developers to create a unique experience for the users.',
+      ],
+    },
+    {
+      company: 'Teaka',
+      companyUrl: '#',
+      role: 'Owner',
+      started: 'December 2022',
+      upto: 'March 2023',
+      tasks: [
+        'Built a community of over 500 people in 3 months.',
+        'Developed a basic box server experience for players.',
+      ],
+    },{
+      company: 'DupeBlocks',
+      companyUrl: '#',
+      role: 'Sr Mod - Owner',
+      started: 'April 2023',
+      upto: 'Present',
+      tasks: [
+        'Built a community of over 5000 people in a year.',
+        'Helped to develop an advanced Lifesteal server with a unique experience. Which brung enjoyment to all players and featured multiple large content creators.',
+        'This helped to bring the server to 350 conncurent players connected at once.',
+        'DupeBlocks enhanced my connections and helped me to become more known in the area',
+      ],
+    }, {
+      company: 'Rivaled',
+      companyUrl: '#',
+      role: 'Owner',
+      started: 'June 2023',
+      upto: 'August 2023',
+      tasks: [
+        'Built a community of over 750 people in 3 months',
+        'I was brought in as a developer to help with the server and ended up taking over the server.',
+        'It was shut down due to the inactivity of the original owner and my time to manage the server.',
+      ],
+    }, {
+      company: 'DupeKitz',
+      companyUrl: '#',
+      role: 'Sr Mod - Admin',
+      started: 'August 2023',
+      upto: 'Present',
+      tasks: [
+        'I was brought in as moderation and quickly started to support development of the community by implementing strict advertising to bring in more players ',
+        'It was shut down due to another administrator leaking the server files.',
+      ],
+    }, {
+      company: 'StealClash',
+      companyUrl: '#',
+      role: 'Admin - Owner',
+      started: 'September 2023',
+      upto: 'Present',
+      tasks: [
+        'I was hired as an admin to help advertisement and to ensure staff were meeting standards',
+        'It was sold to another owner due to the lack of interest and I was then promoted to owner as the person who bought it was unable to manage it.',
+      ],
+    }, {
+      company: 'DupeCrush',
+      companyUrl: '#',
+      role: 'Owner',
+      started: 'September 2023',
+      upto: 'Present',
+      tasks: [
+        'I started DupeCrush to create an enjoyable lifesteal dupe experience for all players',
+        'Over the course of 5 months I built a community of over 1800 people and had a peak of 150 concurrent players',
+        'I sold and then shut down the server after the server was hacked by an administrator',
+      ],
+    }, {
+      company: 'Hardcore',
+      companyUrl: '#',
+      role: 'Developer + Manager',
+      started: 'March 2024',
+      upto: 'Present',
+      tasks: [
+        'I helped with the Development of Hardcore from the very beginning and helped to build a community of over 1500 people',
+        'It was not around for long because due to a lack of attention from management (Including myself) there was a dupe method found that ruined the economy',
+        'The server was shut down due to this but there are plans to bring it back',
+      ],
+    }, {
+      company: 'MineCrush',
+      companyUrl: '#',
+      role: 'Manager + Developer',
+      started: 'March 2024',
+      upto: 'Present',
+      tasks: [
+        'I am a Community Manager at MineCrush and ensure that the community is safe and enjoyable for all players.',
+        'I help maintain that servers are online and that there are no issues related to crashing globally across the network of over 5000 members.',
+        'The server is currently striving with its multiple servers and is well known across Minehut',
+      ],
+    },
+  ],
+};
