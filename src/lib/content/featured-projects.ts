70fdd7e304574da89336314605b2e5ed20d9111f
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-07-23 01:32:49 +0100

    Initial Commit

---
+++ src/lib/content/featured-projects.ts
@@ -0,0 +1,25 @@
+import { FeaturedProjectsSectionType } from '@/lib/types/sections';
+import { getId } from '@/lib/utils/helper';
+
+const featuredProjectsSection: FeaturedProjectsSectionType = {
+  title: "projects i've worked on",
+  projects: [
+    {
+      id: getId(),
+      name: 'EpicLicenses',
+      description: 'An Advanced Java, Kotlin, JavaScript, and Python License Manager.',
+      tasks:
+        "As a freelancer, I understand the importance of ensuring you are paid for your work and being able to revoke the right of access if a Chargeback is made and such. So I created ELicenses a solution to all of the above. Easily managed by Discord Embeds.",
+      url: 'https://elicenses.vercel.app/',
+      img: '#',
+      tags: [
+        'Javascript',
+        'Node.js',
+        'MongoDB',
+        'Redis',
+      ],
+    }
+  ],
+};
+
+export default featuredProjectsSection;
---
+++ src/lib/content/footer.ts
@@ -0,0 +1,6 @@
+import { FooterSectionType } from '@/lib/types/sections';
+
+export const footerSection: FooterSectionType = {
+  title: 'Desgined & Built by SnowyJS',
+  link: 'https://github.com/snowypy/snowy-codes',
+};
