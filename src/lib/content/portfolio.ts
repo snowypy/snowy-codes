501fdb3bdd3167d2793bb15b7ab4388f32257eec
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-07-23 01:34:14 +0100

    Initial Commit

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,36 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  github: 'https://github.com/snowypy',
+  discord: ''
+};
+
+export const author = {
+  name: 'Snowy',
+  email: 'contact@snowy.codes',
+};
+
+export const seoData = {
+  title: 'SnowyJS | Front-End Developer',
+  description:
+    'Snowy is a frontend developer working on Minecraft Optimisation',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://snowy.codes/',
+  keywords: [
+    'Snowy',
+    'SnowyJS',
+    'SnowyH1',
+    'SnowyJavascript',
+    'Portfolio',
+    'Snowy Portfolio ',
+    'SnowyJS Portfolio',
+  ],
+};
