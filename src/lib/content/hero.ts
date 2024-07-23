a6209d08d4f1fcd1a044827cdcc53552b1045d3a
snowy <133208096+snowy-cool@users.noreply.github.com>
2024-07-23 01:32:58 +0100

    Initial Commit

---
+++ src/lib/content/hero.ts
@@ -0,0 +1,16 @@
+import { HeroSectionType } from '@/lib/types/sections';
+import { resumeFileName } from '@/lib/utils/config';
+
+export const heroSection: HeroSectionType = {
+  subtitle: 'Hiya, my name is',
+  title: 'Snowy.',
+  tagline: 'I create optimised web applications.',
+  description:
+    "I am a passionate full-stack web developer with hands-on experience in JavaScript, TypeScript, React.js, Node.js, TailwindCSS, Prisma, and UI/UX design.",
+  specialText: 'Currently available for freelance',
+  cta: {
+    title: 'See My Experience',
+    url: `/#experience`,
+    hideInDesktop: true,
+  },
+};
